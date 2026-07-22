22815424ef411a6845a14a94e3220c53ef3b9b74
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-07-22 17:36:40 -0500

    feat: resuelve ejercicio 1 - calculo de ventas

---
+++ src/Ejercicio1.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class Ejercicio1 {
+    public static void main(String[] args){
+
+        int cantidadVentas;
+        double ValorVenta;
+        double totalVentas = 0;
+
+        cantidadVentas = Integer.parseInt(JOptionPane.showInputDialog("¿Cuantas ventas se realizaron en el dia?"));
+
+        for (int i = 1; i <= cantidadVentas; i++){
+
+            ValorVenta = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor de la venta #" + i));
+            
+            totalVentas = totalVentas + ValorVenta;
+
+        }
+        JOptionPane.showMessageDialog(null,"El total de ventas del dia es: $" + totalVentas );
+    }
+}
